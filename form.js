f54c9937dfa44bf7a2c60bab19eb3cec0f49ec99
Erijona Muji <113674196+Erijonamujii@users.noreply.github.com>
2023-12-04 16:05:02 +0100

    Login form validation
    completed

---
+++ form.js
@@ -0,0 +1,45 @@
+
+
+var myForm=document.forms.loginForm;
+var message=document.getElementById("message");
+
+
+myForm.onsubmit = function(){
+    event.preventDefault();
+
+
+
+
+    if (myForm.username.value == "") {
+        message.innerText = "Please enter the username!";
+        myForm.username.focus();
+        return false;
+    } else if (myForm.password.value == "") {
+        message.innerText = "Please enter the password!";
+        myForm.password.focus();
+        return false;
+    } else {
+        message.innerText = "";
+
+        
+        return true;
+    }
+/*
+if(myForm.username.value ==""){
+    message.innerText = "Please enter the username !";
+    myForm.username.focus();
+    return false;
+}
+else{
+    message.innerText="";
+    return true;
+}
+
+*/
+
+};
+
+
+
+
+
